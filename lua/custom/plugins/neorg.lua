f99457d5d4126b704ec723f2c5820037730381cc
Phạm Quốc Hưng <165866405+LeviathanST@users.noreply.github.com>
2024-08-01 17:39:43 +0700

    add: config dir, concealer

--- lua/custom/plugins/neorg.lua
+++ lua/custom/plugins/neorg.lua
@@ -6,8 +6,27 @@ return {
   },
   {
     'nvim-neorg/neorg',
+    build = ':Neorg sync-parsers',
     lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
     version = '*', -- Pin Neorg to the latest stable release
-    config = true,
+    config = function()
+      require('neorg').setup {
+        load = {
+          ['core.defaults'] = {
+            disable = {},
+          },
+          ['core.concealer'] = {},
+          ['core.dirman'] = {
+            config = {
+              workspaces = {
+                notes = '~/Desktop/Notes',
+              },
+              index = 'index.norg',
+              default_workspace = 'notes',
+            },
+          },
+        },
+      }
+    end,
   },
 }
